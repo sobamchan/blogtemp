71512d5ec2f3aacc5c8d14837d262719ce9db861
sobamchan <oh.sore.sore.soutarou@gmail.com>
2018-12-10 09:44:54 +0900

    use yesterday date by default

--- generate.go
+++ generate.go
@@ -1,48 +1,48 @@
 package main
 
 import (
-    "fmt"
-    "bufio"
-    "os"
-    "time"
-    "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"time"
 )
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func main() {
-    // get title
-    reader := bufio.NewReader(os.Stdin)
-    fmt.Print("Enter title: ")
-    title, _ := reader.ReadString('\n')
-    title = strings.Replace(title, "\n", "", -1)
-    nonSpaceTitle := strings.Replace(title, " ", "-", -1)
+	// get title
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter title: ")
+	title, _ := reader.ReadString('\n')
+	title = strings.Replace(title, "\n", "", -1)
+	nonSpaceTitle := strings.Replace(title, " ", "-", -1)
 
-    // generate time string
-    now := time.Now()
-    nowString := fmt.Sprintf("%d-%02d-%02d", now.Year(), now.Month(), now.Day())
+	// generate time string
+	now := time.Now().AddDate(0, 0, -1)
+	nowString := fmt.Sprintf("%d-%02d-%02d", now.Year(), now.Month(), now.Day())
 
-    // generate filename
-    filename := fmt.Sprintf("%s-%s.md", nowString, nonSpaceTitle)
+	// generate filename
+	filename := fmt.Sprintf("%s-%s.md", nowString, nonSpaceTitle)
 
-    // generate file header
-    header := fmt.Sprintf(`---
+	// generate file header
+	header := fmt.Sprintf(`---
 title: %s
 updated: %d-%02d-%02d %02d:%02d
 ---`, title, now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute())
 
-    // output to file
-    f, err := os.Create(filename)
-    check(err)
+	// output to file
+	f, err := os.Create(filename)
+	check(err)
 
-    defer f.Close()
+	defer f.Close()
 
-    f.Sync()
+	f.Sync()
 
-    f.WriteString(header)
+	f.WriteString(header)
 
 }
